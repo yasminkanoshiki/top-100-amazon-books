758ec4e6ba39ea38a787749753881141e912fb96
Yasmin <yasmin_lie@outlook.com>
2025-08-01 18:55:30 -0300

    Creating Top 100 Amazon books project

---
+++ Top_100_Books.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.set_page_config(layout='wide')
+
+df_reviews = pd.read_csv('C:/Datasets/livros_amazon/customer reviews.csv')
+df_top100_books = pd.read_csv('C:/Datasets/livros_amazon/Top-100 Trending Books.csv')
+
+price_max = df_top100_books['book price'].max()
+price_min = df_top100_books['book price'].min()
+
+st.sidebar.title("Top 100 Bestselling Book Reviews on Amazon")
+
+max_price = st.sidebar.slider('Price Range', price_min, price_max, price_max)
+df_books = df_top100_books[df_top100_books['book price'] <= max_price]
+
+st.write(df_books)
+
+fig = px.bar(df_books['year of publication'].value_counts())
+fig2 = px.histogram(df_books['book price'])
+
+col1, col2 = st.columns(2)
+col1.plotly_chart(fig)
+col2.plotly_chart(fig2)
+
---
+++ pages/Book_Reviews.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout='wide')
+
+df_reviews = pd.read_csv('C:/Datasets/livros_amazon/customer reviews.csv')
+df_top100_books = pd.read_csv('C:/Datasets/livros_amazon/Top-100 Trending Books.csv')
+
+def get_stars(rating):
+    full_stars = int(rating)  
+    half_star = rating - full_stars >= 0.5  
+    stars = "⭐" * full_stars  
+    if half_star:
+        stars += "✩" 
+    return stars
+
+st.sidebar.title("Book Reviews")
+
+books = df_top100_books['book title'].unique()
+book = st.sidebar.selectbox('Books', books)
+
+df_book = df_top100_books[df_top100_books['book title'] == book]
+df_reviews_f = df_reviews[df_reviews['book name'] == book]
+
+book_title = df_book['book title'].iloc[0]
+book_genre = df_book['genre'].iloc[0]
+book_price = f"${df_book['book price'].iloc[0]}"
+book_rating = df_book['rating'].iloc[0]
+book_year = df_book['year of publication'].iloc[0]
+
+st.title(book_title)
+st.subheader(book_genre)
+col1, col2, col3 = st.columns(3)
+col1.metric('Price', book_price)
+col2.metric('Rating', book_rating)
+col3.metric('Year', book_year)
+
+st.divider()
+
+df_books_sem_review = df_top100_books[~df_top100_books["book title"].isin(df_reviews["book name"])]
+
+if book in df_books_sem_review["book title"].values:
+    st.warning("❌ This book has no review yet.")
+else:
+    for row in df_reviews_f.values:
+        rating = float(row[4])  
+        stars = get_stars(rating)  
+
+        st.subheader(f"{stars} ({rating})")
+        st.write(f"**{row[2]}**")  
+        st.write(f"{row[5]}")
+
+
